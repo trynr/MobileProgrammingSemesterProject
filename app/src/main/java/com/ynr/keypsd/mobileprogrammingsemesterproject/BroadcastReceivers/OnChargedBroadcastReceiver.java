35994c34e0ec5b3860a79b2d245c7774c853f683
trynr <ky26222622@hotmail.com>
2022-06-11 01:39:18 +0300

    BroadcastReceiver fixed

--- app/src/main/java/com/ynr/keypsd/mobileprogrammingsemesterproject/BroadcastReceivers/OnChargedBroadcastReceiver.java
+++ app/src/main/java/com/ynr/keypsd/mobileprogrammingsemesterproject/BroadcastReceivers/OnChargedBroadcastReceiver.java
@@ -14,17 +14,12 @@ import com.ynr.keypsd.mobileprogrammingsemesterproject.Helpers.FirebaseDatabaseH
 public class OnChargedBroadcastReceiver extends android.content.BroadcastReceiver  {
     @Override
     public void onReceive(Context context, Intent intent) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Action: " + intent.getAction() + "\n");
 
         int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        boolean isCharging = status == BATTERY_STATUS_CHARGING || status == BATTERY_STATUS_FULL;
+        boolean isCharging = status == BATTERY_STATUS_CHARGING;
 
-        //Toast.makeText(context, "isCharging: " + isCharging, Toast.LENGTH_SHORT).show();
-        return;
-        // TODO kaldır
-        //if(isCharging)
-          //  FirebaseDatabaseHelper.backupMemoriesInFirebaseDatabase(context);
+        if(isCharging)
+            FirebaseDatabaseHelper.backupMemoriesInFirebaseDatabase(context);
 
     }
 }
